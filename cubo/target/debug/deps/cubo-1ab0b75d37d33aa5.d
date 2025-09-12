de3a85205f770e4294c818c4fc43d59427e43a1c
Josero31 <san231221@uvg.edu.gt>
2025-09-12 15:43:45 -0600

    Primeras modificaciones

---
+++ cubo/target/debug/deps/cubo-1ab0b75d37d33aa5.d
@@ -0,0 +1,5 @@
+C:\graficas x computadora\Cubo-con-texturas\cubo\target\debug\deps\cubo-1ab0b75d37d33aa5.d: src\main.rs
+
+C:\graficas x computadora\Cubo-con-texturas\cubo\target\debug\deps\libcubo-1ab0b75d37d33aa5.rmeta: src\main.rs
+
+src\main.rs:
---
+++ cubo/target/debug/deps/cubo-b8bfbe11e43c31d2.d
@@ -0,0 +1,5 @@
+C:\graficas x computadora\Cubo-con-texturas\cubo\target\debug\deps\cubo-b8bfbe11e43c31d2.d: src\main.rs
+
+C:\graficas x computadora\Cubo-con-texturas\cubo\target\debug\deps\libcubo-b8bfbe11e43c31d2.rmeta: src\main.rs
+
+src\main.rs:
